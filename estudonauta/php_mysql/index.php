a685f334da0d88d5ff0f6c7dd145d7c2ee59c7e5
evertonklegin <evertonklegin@gmail.com>
2023-08-29 15:53:55 -0300

    M1A09

--- estudonauta/php_mysql/index.php
+++ estudonauta/php_mysql/index.php
@@ -11,23 +11,32 @@
         require_once "includes/banco.php";
         require_once "includes/funcoes.php";
         $ordem = $_GET['o'] ?? "n";
+        $chave = $_GET['c'] ?? "";
     ?>
     <div id="corpo">
         <?php include_once "topo.php"?>
+        
         <h1>Escolha seu Jogo</h1>
+        
         <form action="index.php" method="get" id="busca">
             Ordenar: 
             <a href="index.php?o=n">Nome | </a>
             <a href="index.php?o=p">Produtora |</a> 
             <a href="index.php?o=n1">Nota Alta |</a> 
             <a href="index.php?o=n2">Nota Baixa |</a>
+            <a href="index.php">Mostrar Todos |</a>
             <label for="c">Buscar:</label>
             <input type="text" name="c" id="c" size="10" maxlength="40">
             <input type="submit" value="Ok">
         </form>
+        
         <table class="listagem">
             <?php 
                 $q = "select j.cod, j.nome, g.genero, p.produtora, j.capa from jogos j join generos g on j.genero = g.cod join produtoras p on j.produtora = p.cod ";
+
+                if (!empty($chave)) {
+                    $q .= "WHERE j.nome like '%$chave%' OR p.produtora like '%$chave%' OR g.genero like '%$chave%' ";
+                }
 
                 switch ($ordem) {
                     case "p":
